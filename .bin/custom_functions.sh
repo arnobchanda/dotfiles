3b266b91e4c3a96bdbe5c387be67eba6b21f271e
Arnob Chanda <arnob.chanda@aesi-ess.com>
2025-03-25 13:34:35 +0000

    Updating custom_functions and adding tmux conf

--- .bin/custom_functions.sh
+++ .bin/custom_functions.sh
@@ -1,7 +1,20 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-function fix_path()
-{
+fix_path() {
     LINUX_PATH=$(/usr/bin/printenv PATH | /usr/bin/perl -ne 'print join(":", grep { !/\/mnt\/[a-z]/ } split(/:/));')
     export PATH=$LINUX_PATH
+}
+
+cargo-update() {
+    echo "Updating all cargo packages"
+    installed_crates=$(cargo install --list | grep -E '^[a-z0-9_-]+ v[0-9.]+:$' | awk '{print $1}')
+    for crate in $installed_crates; do
+        cargo install "$crate"
+    done
+}
+
+update-all-packages() {
+    sudo apt update && sudo apt upgrade -y
+    brew update && brew upgrade
+    cargo-update
 }
